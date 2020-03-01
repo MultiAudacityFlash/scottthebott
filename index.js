cf49d973618741afc23febf7f4719f03ea5d5fa5
BuildTools <unconfigured@null.spigotmc.org>
2020-03-01 18:53:52 -0500

    pre-heroku

--- index.js
+++ index.js
@@ -233,10 +233,19 @@ https://pbs.twimg.com/ext_tw_video_thumb/1139216636697894912/pu/img/_y-UE9Z7_zYJ
     if(msg.content.includes("buy house button")){
         msg.channel.sendMessage(`I BOUGHT F--KING TWENTY OF THEM.`);
     }
+    if(msg.content.includes("Buy house button")){
+        msg.channel.sendMessage(`I BOUGHT F--KING TWENTY OF THEM.`);
+    }
     if(msg.content.includes("buy-house-button")){
         msg.channel.sendMessage(`I BOUGHT F--KING TWENTY OF THEM.`);
     }
+    if(msg.content.includes("Buy-house-button")){
+        msg.channel.sendMessage(`I BOUGHT F--KING TWENTY OF THEM.`);
+    }
     if(msg.content.includes("buy-house button")){
+        msg.channel.sendMessage(`I BOUGHT F--KING TWENTY OF THEM.`);
+    }
+    if(msg.content.includes("Buy-house button")){
         msg.channel.sendMessage(`I BOUGHT F--KING TWENTY OF THEM.`);
     }
     if(msg.content.includes("Buy House Button")){
